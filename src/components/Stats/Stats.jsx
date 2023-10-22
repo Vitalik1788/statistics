96a6ecf5493741d13f58d9e8385da897ffafd7e9
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-10-22 22:40:16 +0300

    up

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -8,58 +8,73 @@ import {
   Tooltip,
   Legend,
   LabelList,
+  Line,
 } from 'recharts';
 import { StatsContainer, StatsPageBox } from './Stats.styled';
 
 const data = [
   {
-    name: 'To Do',
-    'By Day': 35,
-    'By Month': 24,
+    name:'To Do',
+    'By Day': 75,
+    'By Month': 44,
   },
   {
-    name: 'In Progress',
-    'By Day': 40,
-    'By Month': 24,
+    name:'In Progress',
+    'By Day': 60,
+    'By Month': 64,
   },
   {
-    name: 'Done',
-    'By Day': 40,
-    'By Month': 24,
+    name:'Done',
+    'By Day': 60,
+    'By Month': 44,
   },
 ];
 
 export default function App() {
   return (
     <StatsPageBox>
-      <button>Calendar</button>
-
       <StatsContainer>
         <BarChart
-          width={780}
-          height={440}
+          width={1087}
+          height={752}
           data={data}
           margin={{ top: 40, right: 40, left: 40, bottom: 40 }}
         >
-          <CartesianGrid vertical={false} />
-          <XAxis dataKey="name" axisLine={false} tickLine={false} />
+          <CartesianGrid vertical={false} stroke="#E3F3FF" />
+          <XAxis
+            dataKey="name"
+            axisLine={false}
+            tickLine={false}
+            fontSize={14}
+            fontFamily="Inter"
+            tickMargin={20}
+          />
           <YAxis
             domain={[0, 100]}
             tickCount={6}
             axisLine={false}
             tickLine={false}
-            label={{ value: 'Tasks', position: 'top' }}
+            label={{
+              value: 'Tasks',
+              position: 'top',
+              dx: -30,
+              fontFamily: 'Inter',
+              fontSize: 14,
+              fontWeight: 600,
+            }}
             padding={{ top: 80 }}
             tickMargin={50}
           />
-          <Tooltip />
+          {/* <Tooltip /> */}
           <Legend
+            wrapperStyle={{ position: 'absolute', top: -50 }}
             layout="horizontal"
             verticalAlign="top"
             align="end"
             iconSize={8}
             iconType="circle"
             markerWidth={5}
+            fontSize={16}
           />
           <Bar dataKey="By Day" fill="#FFD2DD" barSize={27} radius={10}>
             <LabelList dataKey="By Day" position="insideTop" />
--- src/components/Stats/Stats.styled.jsx
+++ src/components/Stats/Stats.styled.jsx
@@ -1,16 +1,15 @@
 import styled from "@emotion/styled";
 
-export const StatsPageBox = styled.div`
-  height: 752px;
-  width: 1087px;
+export const StatsPageBox = styled.div`  
+  width: 100vh;
 `;
 
 export const StatsContainer = styled.div`
-  width: 100vh;
+  width: 1087px;
   height: auto;
   margin-left: auto;
   margin-right: auto;
-  margin-top: 100px;
+  margin-top: 150px;
   border: 0.8px solid #e3f3ff;
   border-radius: 29px;
 `;
